5852f0a78b55413917286e896626ac4492ec56a2
ruibs <r.bartolome.2022@alumnos.urjc.es>
2025-10-24 12:48:41 +0200

    si

--- cyclictestURJC.c
+++ cyclictestURJC.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,6 +8,7 @@
 #include <fcntl.h>
 #include <time.h>
 #include <pthread.h>
+#include <sched.h>
 
 #ifdef DEBUG
 #define DEBUG_PRINTF(...) printf("DEBUG: "__VA_ARGS__)
@@ -15,62 +17,88 @@
 #endif
 
 /*Global variables*/
-#define SLEEP_TIME 100 // Sleep time
-#define EXEC_TIME 5 // Execution time
+#define SLEEP_TIME 1 // Sleep time will convert in ms
+#define EXEC_TIME 60 // Execution time
+bool time_flag = true; // Flag to know when the time has passed, true->runnning, false->finished
 
 typedef struct {
     int id;
-    double *av_latencies;
-    double *max_latencies;
+    long long **latencies;
 } ThreadArgs;
 
-double get_time_diif(const struct timespec *t1, const struct timespec *t2){
+// Return the difference between t2 and t1 in ns
+long long get_time_diff(const struct timespec *t1, const struct timespec *t2){
     long sec_diff = t2->tv_sec - t1->tv_sec;
     long nsec_diff = t2->tv_nsec - t1->tv_nsec;
 
-    return (sec_diff * 1000.0) + (nsec_diff / 1e6);
+    // If the diff of ns is lower than 0 adjust.
+    if (nsec_diff < 0) {
+        sec_diff -= 1;
+        nsec_diff += 1000000000L;
+    }
+
+    long long time = (sec_diff * 1000000000LL) + (nsec_diff);
+    return time;
 }
 
 // Function to treat each client in a thread
 void* thread_core(void *arg) {
+    // Set prio
+    struct sched_param param;
+    param.sched_priority = 99;
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+
     ThreadArgs *args = (ThreadArgs *)arg; // Cast from void to my struc
 
-    double latency = 0, sum = 0, max_latency = 0;
+    long long latency = 0;
+    long long *latencies = malloc(sizeof(long long) * 10000); // some large number or estimate
+    if (!latencies) {
+        perror("malloc");
+        pthread_exit(NULL);
+    }
+
+
     struct timespec t1, t2;
 
     int num_iter = 0;
-    while (1) {
-        // get actual time -> sleep -> get future time and check how much has desviated from the sleep
-        clock_gettime(CLOCK_MONOTONIC, &t1);
-        usleep(SLEEP_TIME);
-        clock_gettime(CLOCK_MONOTONIC, &t2);
+    while (time_flag) {
+        // get actual time -> sleep -> get future time and check how much has desviated from the time sleep
+        clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
 
-        double time_diff = get_time_diif(&t1, &t2);
-        latency = time_diff - SLEEP_TIME;
+        struct timespec ts;
+        ts.tv_sec = 0;
+        ts.tv_nsec = SLEEP_TIME * 1000000; // SLEEP_TIME in ms
+        nanosleep(&ts, NULL);
 
-        // I the sleep was sorter than the time expected then the latency is perfect = 0
+        clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
+
+        long long time_diff = get_time_diff(&t1, &t2);
+        DEBUG_PRINTF("Diff ms %lld, id [%d]\n", time_diff, args->id);
+
+        latency = time_diff - SLEEP_TIME * 1000000LL;
+        DEBUG_PRINTF("Diff seg %lld\n", latency);
+
+        // If the sleep was sorter than the time expected then the latency is perfect = 0
         if (latency <  0)
             latency = 0;
 
-        // Check if the latency is the highest until now
-        if (latency > max_latency)
-            args->max_latencies[args->id] = latency;
+        latencies[num_iter] = latency;
+        num_iter ++;
 
-        sum += latency; // Add the actual value to the sum of the previous latencies
-        num_iter++; // Increase the iteration number to make the average
-
-        args->av_latencies[args->id] = sum / num_iter;
     }
-
+    // Añadir al array latencies
+    args->latencies[args->id] = latencies;
+    free(latencies);
+    return 0;
 }
 
 
 int main (int argc, char* argv[]) {
     int N = (int) sysconf(_SC_NPROCESSORS_ONLN); // Number of cores
-    DEBUG_PRINTF("%d",N);
+    DEBUG_PRINTF("Num of cores %d\n",N);
 
-    double av_latencies[N]; // Array to store the average latencie of each core
-    double max_latencies[N]; // Array to store the maximun latencie of each core
+    long long latencies[N]; // Array to store the average latencie of each core
+    //long long max_latencies[N]; // Array to store the maximun latencie of each core
     pthread_t threads[N]; // Array to store the threads id
 
     static int32_t latency_target_value = 0;
@@ -84,35 +112,52 @@ int main (int argc, char* argv[]) {
         ThreadArgs* args = malloc(sizeof(ThreadArgs));
 
         args->id = i;
-        args->av_latencies = av_latencies;
-        args->max_latencies = max_latencies;
+        args->latencies = latencies;
+        //args->max_latencies = max_latencies;
 
         pthread_create(&thread, NULL, thread_core, args);
+
+        // Set afinity
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(i, &cpuset);
+        pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+
         threads[i] = thread;
     }
     sleep(EXEC_TIME);
+    time_flag = false;
+
     for (int i = 0; i < N; i++) {
         pthread_join(threads[i], NULL);
     }
 
+    /*
+    // Check if the latency is the highest until now
+    if (latency > max_latency)
+        args->max_latencies[args->id] = latency;
 
+    sum += latency; // Add the actual value to the sum of the previous latencies
+    num_iter++; // Increase the iteration number to make the average
+
+    args->av_latencies[args->id] = sum / num_iter;
     
-    
+
     // Print results
-    double total_av = 0;
-    double total_max = 0;
+    long long total_av = 0;
+    long long total_max = 0;
     for(int x = 0; x < N; x++) {
-        printf("[%d] latencia media = %f ns. | max = %f ns", x, av_latencies[x], max_latencies[x]);
+        printf("[%d] latencia media = %lld ns. | max = %lld ns\n", x, av_latencies[x], max_latencies[x]);
         
         total_av += av_latencies[x];
-        if (total_max < av_latencies[x])
-            total_max = av_latencies[x];
+        if (total_max < max_latencies[x])
+            total_max = max_latencies[x];
     }
-    printf("Total latencia media = %f ns. | max = %f ns", total_av, total_max);
-    
+    printf("Total latencia media = %lld ns. | max = %lld ns\n", total_av / N, total_max);
+    */
     // Close cpu_dma_latency
     if (close(latency_target_fd) == -1) {
-        perror("Error closing file");
+        perror("Error closing file\n");
         return -1;
     }
     return 0;
